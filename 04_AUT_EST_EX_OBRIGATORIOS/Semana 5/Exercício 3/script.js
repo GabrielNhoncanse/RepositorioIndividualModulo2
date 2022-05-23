80b81dad36b01f418ffded6bb82b75d2f8b75347
GabrielNhoncanse <gabriel.nhoncanse@sou.inteli.edu.br>
2022-05-23 10:15:17 -0300

    Exercícios da semana 5

--- 04_AUT_EST_EX_OBRIGATORIOS/Semana 5/Exercício 3/script.js
+++ 04_AUT_EST_EX_OBRIGATORIOS/Semana 5/Exercício 3/script.js
@@ -1,5 +1,18 @@
 function ordenar() {
+    var listaoriginal = document.getElementById("lista").value
     var lista = document.getElementById("lista")
     var listare = lista.value.split(",")
-
+    for (let j = 0; j < listare.length - 1; j++) {
+        for (let i = 0; i < listare.length - 1; i++) {
+            listare[i] = parseInt(listare[i])
+            listare[i + 1] = parseInt(listare[i + 1])
+            if (listare[i] >= listare[i + 1]) {
+                let dado = listare[i + 1]
+                listare[i + 1] = listare[i]
+                listare[i] = dado
+            }
+        }
+    }
+    document.getElementById("desordenado").innerHTML = "Lista dada = " + listaoriginal;
+    document.getElementById('resultado').innerHTML = "Lista em ordem crescente = " + listare;
 }
