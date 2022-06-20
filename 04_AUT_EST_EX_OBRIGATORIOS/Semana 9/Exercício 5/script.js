030c5f87f37d66817b3146bc6c6f69cac5d8a2fe
GabrielNhoncanse <gabriel.nhoncanse@sou.inteli.edu.br>
2022-06-20 15:24:48 -0300

    Exercício 5 Semana 9

--- 04_AUT_EST_EX_OBRIGATORIOS/Semana 9/Exercício 5/script.js
+++ 04_AUT_EST_EX_OBRIGATORIOS/Semana 9/Exercício 5/script.js
@@ -1,25 +1,26 @@
 function calc() {
     var valor1 = parseInt(document.getElementById("valor1").value);
     var valor2 = parseInt(document.getElementById("valor2").value);
-    var lista = [valor1];
-    for (var j = valor1; j < valor2; j++) { //aumentar tamanho da lista
-        aux = valor1 + 1;
-        valor1 = aux;
-        lista.push(aux);
-    }
+    // var lista = [valor1];
+    // for (var j = valor1; j < valor2; j++) { //aumentar tamanho da lista
+    //     aux = valor1 + 1;
+    //     valor1 = aux;
+    //     lista.push(aux);
+    // }
 
     var listaprimos = [];
-    var divisores = 0;
-    for (var t = 0; t <= lista.length; t++) { //selecionar todos os numeros da lista
-        for (var i = 0; i <= lista[t]; i++) {//verificar se o numero selecionado é primo
-            if ((lista[t] % i) == 0) {
+
+    for (var t = valor1; t <= valor2; t++) { //selecionar todos os numeros da lista
+        var divisores = 0;
+        for (var i = 2; i < t; i++) { //verificar se o numero selecionado é primo
+            if ((t % i) == 0) {
                 divisores++;
-            }
-            if (divisores == 2) {
-                listaprimos.push(lista[t]);
-            }
-        }
-    }
-    //console.log(lista);
-    console.log(listaprimos);
-}
+            };
+        };
+
+        if (divisores == 0 && t != 1) {
+            listaprimos.push(t);
+        };
+    };
+    document.getElementById("res").innerHTML = listaprimos
+};
